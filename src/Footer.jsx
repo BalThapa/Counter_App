9bf23110fa84b4191ef90386f75178dc792df681
BalThapa <info.balthapa@gmail.com>
2023-03-29 22:41:45 +0300

    counter-app WIP

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <small>Copyright</small>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Main.jsx
@@ -0,0 +1,73 @@
+import React, {Component} from "react";
+import "./Main.css";
+import Button from './UI/Button'
+
+
+class Main extends Component {
+  state = {
+    counter: 0,
+  };
+
+  addFiveHandler = () => {
+    this.setState({
+      counter:this.state.counter + 5,
+    });
+  };
+  addOneHandler = () => {
+    this.setState({
+      counter:this.state.counter + 1,
+    });
+  };
+
+  resetHandler = () => {
+    this.setState({
+    counter:0,
+    });
+  };
+
+  removeFiveHandler = () => {
+    this.setState({
+      counter:this.state.counter -5,
+    });
+  };
+
+  removeOneHandler = () => {
+    this.setState({
+      counter:this.state.counter -1,
+    });
+  };
+
+  render (){
+    let circleClass = ' ' ;
+
+    if (this.state.counter === 0) {
+        circleClass = ' ';
+
+    } else if (this.state.counter % 2 === 0) {
+        circleClass = 'red';
+        
+    } else {
+        circleClass = 'yellow';
+        
+    }
+
+    return (
+      <div className="main">
+        <div className="circle">
+          <h1 className={circleClass}>{this.state.counter}</h1>
+        </div>
+        <div className="button">
+          <Button click={this.addFiveHandler} text="Add 5"></Button>
+          <Button click={this.addOneHandler} text="Add 1"></Button>
+          <Button  click={this.resetHandler} text="Reset"></Button>
+          <Button  click={this.removeFiveHandler} text="Remove 5"></Button>
+          <Button click={this.removeOneHandler}  text="Remove 1"></Button> 
+        </div>
+      </div>
+    );
+  }  
+    
+
+}
+
+export default Main;
